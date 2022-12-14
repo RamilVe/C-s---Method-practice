33580a50568ab31ec1f11bb12bebc6e1fe60a99b
Ramil <ramilev@code.edu.az>
2022-12-14 12:57:13 +0400

    homework added

---
+++ Cs - Method practice tasks/Cs - Method practice tasks/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Reflection;
+
+namespace Cs___Method_practice_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            int[] numbers = { 12, 26, 45 };
+            Console.WriteLine(FindBiggest(numbers));
+            #endregion
+
+
+            #region Task4
+            //Console.WriteLine("Yazini daxil edin: ");
+            //string text = Console.ReadLine();
+            //char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            //Console.WriteLine(HasNum(digits, text));
+            #endregion
+
+
+
+            #region Task 5
+            //string[] texts = { "Ram1l", "T0fiq", "Abbas", "N4rm7n" };
+            //Console.WriteLine(CountNum(digits, texts));
+            #endregion
+
+        }
+
+        //- Verilmiş ədədlər siyahısındaki ən böyük ədədin yerləşdiyi index-i qaytaran metod
+        static int FindBiggest(int[] numbers)
+        {
+            int max = numbers[0];
+            int index = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        //- Verilmiş yazılar siyahısındaki(string array) verilmiş yazının yerləşdiyi indexi qaytaran metod.Əgər verilmiş string array-de verilmiş string dəyər yoxdursa -1 qaytarsın
+        //- Verilmiş ədədlər siyahısındaki ədədlərin yalnız müsbət olanlarından ibarət yeni bir array qaytaran metod.Yəni metodu işə salın { 4,-5,3,-2,6}ədədlərindən ibarət array göndərsəm metod geriyə {4,3,6} ədədlərindən ibarət array qaytarmalıdır
+        //- Verilmiş yazıda rəqəm olub olmadığını qaytaran metod
+        static bool HasNum(char[] digits, string text)
+        {
+           bool hasDigit = false;
+           for(int i = 0; i < text.Length;i++)
+            {
+                for(int j = 0; j < digits.Length; j++)
+                {
+                    if (text[i] == digits[j])
+                    {
+                        hasDigit = true;
+                        break;
+                    }
+                }
+            } 
+           return hasDigit;
+        }
+
+        //- Verilmiş yazılar siyahısında neçə yazıda rəqəm olduğunu qaytaran metod
+        static int CountNum(char[] digits, string[] texts)
+        {
+            int count = 0;
+            for (int i = 0; i < texts.Length; i++)
+            {
+                for (int j = 0; j < digits.Length; j++)
+                {
+                    for (int n = 0; n < texts[i].Length; n++)
+                    {
+                        if (texts[i][n] == digits[j])
+                        {
+                            count++;
+                            break;
+                        }
+                    }
+                    
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Cs - Method practice/Cs - Method practice/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Cs___Method_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Eded: ");
+            //string numStr = Console.ReadLine();
+            //int num = Convert.ToInt32(numStr);
+
+            //Console.WriteLine("Quvvet: ");
+            //string powerStr = Console.ReadLine();
+            //int power = Convert.ToInt32(powerStr);
+ 
+            //Console.WriteLine(Pow(num, power));
+
+            int[] numbers = { 10,12,13 };
+            Console.WriteLine(Sum(numbers));
+
+        }
+
+
+        //- Verilmiş ədədin verilmiş qüvvətini tapan metod
+        //static int Pow(int num, int power)
+        //{
+        //    int result = 1;
+        //    for (int i = 0; i < power; i++)
+        //    {
+        //        result *= num;
+        //    }
+        //    return result;
+        //}
+
+        //- Verilmiş 3 ədəddən ən böyüyünü tapan metod
+        //static int FindBiggest(int num1, int num2, int num3)
+        //{
+        //    if (num1 > num2)
+        //    {
+        //        if(num1 > num3)
+        //        {
+        //            return num1;
+        //        }
+        //        else
+        //        {
+        //            return num3;
+        //        }
+        //    }
+        //    else if (num2 > num3)
+        //    {
+        //        return num2;
+        //    }
+        //    else
+        //    {
+        //        return num3;
+        //    }
+        //}
+
+        // - Verilmiş ədədlər siyahısındaki ən böyük ədədi tapan metod
+        //static int FindBiggest(int[] numbers)
+        //{
+        //    int max = numbers[0];
+        //    for(int i = 0; i < numbers.Length; i++)
+        //    {
+        //        if (numbers[i] > max)
+        //        {
+        //            max = numbers[i]; 
+        //        }
+        //    }
+
+        //    return max;
+        //}
+
+        // - Verilmiş ədədlər siyahısındaki ədədlərin cəmini tapan metod
+        static int Sum(int[] numbers)
+        {
+            var sum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
